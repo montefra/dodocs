9deef91a84b472989b32b0821ebc6744a18af563
Francesco Montesano <franz.bergesund@gmail.com>
2015-03-03 19:08:19 +0100

    get the version number

---
+++ dodoc/utils.py
@@ -0,0 +1,24 @@
+"""Generic utilities"""
+
+
+def get_version(from_file=None):
+    """
+    Returns the version number.
+
+    Inspired by from :mod:`matplotlib.setupext`
+
+    Parameters
+    ----------
+    from_file: string, optional
+        scan from the string ``__version__`` in the given file
+    """
+    if from_file is None:
+        import dodoc
+        version = dodoc.__version__
+    else:
+        with open('lib/__init__.py') as fd:
+            for line in fd:
+                if (line.startswith('__version__')):
+                    version = line.split('=')[1].strip().strip('"')
+                    break
+    return version
