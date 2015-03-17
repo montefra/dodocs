641cc7d566c4983bc0a79caf204674005d44abab
Francesco Montesano <franz.bergesund@gmail.com>
2015-03-17 22:47:54 +0100

    config parser added

--- dodocs/config.py
+++ dodocs/config.py
@@ -11,9 +11,17 @@ MIT Licence
 import configparser as confp
 import os
 
+import dodocs.utils as du
+
+# name of the configuration file
+CONF_FILE = "dodocs_setup.cfg"
+
+# private dictionary storing configurations
+_config_dic = {}
+
 
 def get_sample_cfg_file():
-    """Get the name of the sample configuration file
+    """Get the name of the sample configuration file.
 
     Returns
     -------
@@ -21,5 +29,31 @@ def get_sample_cfg_file():
         absolute path of the sample configuration file
     """
     path = os.path.split(os.path.abspath(__file__))[0]
-    cfg_file = os.path.join(path, "dodocs_data", "dodocs_setup.cfg")
+    cfg_file = os.path.join(path, "dodocs_data", CONF_FILE)
     return cfg_file
+
+
+@du.format_docstring(CONF_FILE)
+def get_config(profile):
+    """Returns the configuration for the given profile. If it doesn't exists,
+    first create it.
+
+    It is assumed the existence of the file {} under the ``profile`` directory.
+
+    Parameters
+    ----------
+    profile : string
+        name of the profile
+
+    Returns
+    -------
+    conf : :class:`confp.ConfigParser` instance
+    """
+    try:
+        return _config_dic[profile]
+    except KeyError:
+        conf = confp.ConfigParser()
+        fname = os.path.join(du.dodocs_directory(), profile, CONF_FILE)
+        conf.read(fname)
+        _config_dic[profile] = conf
+        return conf
