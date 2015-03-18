8f88c9b8299e3ffc0f4879d87496efdef8ab5d2a
Francesco Montesano <franz.bergesund@gmail.com>
2015-03-18 09:25:15 +0100

    Check that the config file has been modified

--- dodocs/config.py
+++ dodocs/config.py
@@ -11,7 +11,7 @@ MIT Licence
 import configparser as confp
 import os
 
-import colorama 
+import colorama
 
 import dodocs.utils as du
 
@@ -58,13 +58,14 @@ def get_config(profile):
         fname = os.path.join(du.dodocs_directory(), profile, CONF_FILE)
         conf.read(fname)
 
-        check_version(conf)
+        check_edited(conf, profile)
+        check_version(conf, profile)
 
         _config_dic[profile] = conf
         return conf
 
 
-def check_version(conf):
+def check_version(conf, profile):
     """Check that the version in the configuration files corresponds with the
     one of ``dodocs``
 
@@ -72,20 +73,42 @@ def check_version(conf):
     ----------
     conf : :class:`confp.ConfigParser` instance
         configuration object
+    profile : string
+        name of the profile
     """
     try:
         conf_version = conf.get("general", "version")
         current_version = du.get_version()
         if conf_version != current_version:
-            msg = ("The current `dodocs` version {dv} is different from the"
-                   " one in the configuration file {dc}. Beware that the built"
-                   " might fail."
+            msg = ("[{p}] The current `dodocs` version {dv} is different from"
+                   " the one in the configuration file {dc}. Beware that the"
+                   " built might fail."
                    )
             print(colorama.Fore.YELLOW + msg.format(dv=current_version,
-                                                    dc=conf_version))
+                                                    dc=conf_version,
+                                                    p=profile))
     except confp.NoOptionError:
-        msg = ("The configuration file doesn't have a `version` entry. Are you"
-               " sure that it is a `dodocs` configuration"
+        msg = ("[{p}] The configuration file doesn't have a `version` entry."
+               " Are you sure that it is a `dodocs` configuration"
                )
-        print(colorama.Fore.RED + msg)
+        print(colorama.Fore.RED + msg.format(p=profile))
         raise
+
+
+def check_edited(conf, profile):
+    """Check if the configuration file has been edited.
+
+    Parameters
+    ----------
+    conf : :class:`confp.ConfigParser` instance
+        configuration object
+    profile : string
+        name of the profile
+    """
+    try:
+        if conf.getboolean('general', 'is_edited'):
+            msg = ("[{p}] The configuration file has likely not been edited."
+                   " Aborting")
+            print(colorama.Fore.RED + msg.format(p=profile))
+    except confp.NoOptionError:
+        pass
