e2cef7a81dbd83a5272dd5baa806979f94db3844
Francesco Montesano <franz.bergesund@gmail.com>
2015-03-26 09:19:15 +0100

    List projects in profiles

--- dodocs/profiles/plist.py
+++ dodocs/profiles/plist.py
@@ -8,6 +8,7 @@ import os
 
 import colorama
 
+import dodocs.config as dconf
 from dodocs import utils
 from dodocs.logger import getLogger
 
@@ -26,12 +27,20 @@ def plist(args):
     dodocs_dir = utils.dodocs_directory()
     dirpath, dirnames = next(os.walk(dodocs_dir))[:2]
     if dirnames:
-        print(colorama.Fore.GREEN + "Available profiles:")
+        msg = colorama.Fore.GREEN + "Available profiles:" + colorama.Fore.RESET
         for d in dirnames:
             profile_dir = os.path.join(dodocs_dir, d)
-            msg = "  * {}".format(d)
+            msg += "\n  * {}".format(d)
             if os.path.islink(profile_dir):
                 msg += " (-> {})".format(os.path.realpath(profile_dir))
-            print(msg)
+            try:
+                for project in dconf.get_projects(d):
+                    msg += "\n    + {}".format(project)
+            except dconf.DodocConfigError as e:
+                msg += ("\n    + " + colorama.Fore.RED + "there is a problem"
+                        "with the configuration file" + colorama.Fore.RESET)
+                log.error(e)
+
+        print(msg)
     else:
         print(colorama.Fore.RED + "No profile found")
