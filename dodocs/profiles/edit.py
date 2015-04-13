6afaf7de702cfacc589277f577b1ede0563839f2
Francesco Montesano <franz.bergesund@gmail.com>
2015-04-13 18:49:20 +0200

    edit profile

--- dodocs/profiles/edit.py
+++ dodocs/profiles/edit.py
@@ -1,10 +1,16 @@
-"""Profile creation and management
-
-This module provides the command line and the entry point for the sub-package
+"""Edit the given profile
 
 Copyright (c) 2015 Francesco Montesano
 MIT Licence
 """
+
+import os
+import subprocess as sp
+import sys
+
+import dodocs.config as dconf
+from dodocs.logger import getLogger
+from dodocs import utils
 
 
 def edit(args):
@@ -15,4 +21,21 @@ def edit(args):
     args : namespace
         parsed command line arguments
     """
-    pass
+    log = getLogger()
+
+    log.debug("opening configuration file for profile {} for"
+              " editing".format(args.name))
+
+    dodocs_dir = utils.dodocs_directory()
+    config_file = os.path.join(dodocs_dir, args.name, dconf.CONF_FILE)
+
+    # this part is taken without shame from the web
+    if os.name == 'posix':
+        if "EDITOR" in os.environ:
+            sp.call([os.environ["EDITOR"], config_file])
+        else:
+            sp.call(["xdg-open", config_file])
+    elif sys.platform.startswith('darwin'):
+        sp.call(["open", config_file])
+    elif os.name == 'nt':
+        os.startfile(config_file)
