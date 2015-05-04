e93f0b523643a3b403e3f84809e7866f9facce04
Francesco Montesano <franz.bergesund@gmail.com>
2015-05-04 08:41:02 +0200

    Adapt to use with libpath

--- dodocs/profiles/create.py
+++ dodocs/profiles/create.py
@@ -5,6 +5,7 @@ MIT Licence
 """
 
 import os
+from pathlib import Path
 import shutil
 import string
 import warnings
@@ -29,34 +30,35 @@ def create(args):
 
     for name in args.name:
         log.info("Creating profile {}".format(name))
-        profile_dir = os.path.join(dodocs_dir, name)
+        profile_dir = dutils.profile_dir(name)
         log.debug("Profile directory: {}".format(profile_dir))
 
         # deal with existing profile
-        if os.path.exists(profile_dir):
-            if args.force and (os.path.isdir(profile_dir) or
-                               os.path.isfile(profile_dir)):
-                log.warning(colorama.Fore.YELLOW +
-                            "Removing '{}'".format(name))
-                shutil.rmtree(profile_dir)
-            elif args.force and os.path.islink(profile_dir):
+        if profile_dir.exists():
+            if args.force and profile_dir.is_symlink():
                 log.warning(colorama.Fore.YELLOW +
                             "Unlinking and Removing '{}'".format(name))
-                realpath = os.path.realpath(profile_dir)
-                os.remove(profile_dir)
-                shutil.rmtree(realpath)
+                realpath = profile_dir.resolve()
+                profile_dir.unlink()
+                shutil.rmtree(str(realpath))
+            elif args.force and (profile_dir.is_dir() or
+                                 profile_dir.is_file()):
+                log.warning(colorama.Fore.YELLOW +
+                            "Removing '{}'".format(name))
+                shutil.rmtree(str(profile_dir))
             else:
-                msg = colorama.Fore.RED + "Profile '{pd}' already exists. Aborting."
+                msg = colorama.Fore.RED
+                msg += "Profile '{pd}' already exists. Aborting."
                 log.error(msg.format(pd=name))
                 continue
 
         # create new profiles
         if args.link:
             link_dir = profile_dir
-            profile_dir = os.path.join(args.link, name)
+            profile_dir = Path(args.link).resolve() / name
             log.debug("Profile linked to {}".format(profile_dir))
         try:
-            os.mkdir(profile_dir)
+            profile_dir.mkdir(parents=True)
         except FileExistsError:
             log.error(colorama.Fore.RED +
                       "'{pd}' already exists. Aborting.")
@@ -64,7 +66,7 @@ def create(args):
 
         copy_config(profile_dir)
         if args.link:
-            os.symlink(profile_dir, link_dir)
+            link_dir.symlink_to(profile_dir, target_is_directory=True)
         log.info(colorama.Fore.GREEN + "profile '{}' created".format(name))
 
 
@@ -82,8 +84,8 @@ def copy_config(profile_dir):
     cfg_file = dconf.get_sample_cfg_file()
     log.debug("Copy file '{}' into directory '{}'".format(cfg_file,
                                                           profile_dir))
-    with open(cfg_file, 'r') as inf,\
-                open(os.path.join(profile_dir, dconf.CONF_FILE), 'w') as outf:
+    with cfg_file.open('r') as inf, \
+                (profile_dir / dconf.CONF_FILE).open('w') as outf:
         conf = string.Template(inf.read())
         conf = conf.safe_substitute(version=dutils.get_version())
         outf.write(conf)
