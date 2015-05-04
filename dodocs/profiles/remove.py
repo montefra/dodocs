3c99775fb92d8b635fb34c37b58ec920ed6542af
Francesco Montesano <franz.bergesund@gmail.com>
2015-05-04 08:44:12 +0200

    Adapt to use with pathlib

--- dodocs/profiles/remove.py
+++ dodocs/profiles/remove.py
@@ -27,17 +27,17 @@ def remove(args):
         log.debug("Removing profile {}".format(name))
         profile_dir = dutils.profile_dir(name)
 
-        if not os.path.exists(profile_dir):
+        if not profile_dir.exists():
             log.warn("Profile {} does not exist".format(name))
             continue
 
         try:
-            if os.path.islink(profile_dir):
-                realpath = os.path.realpath(profile_dir)
-                os.remove(profile_dir)
-                shutil.rmtree(realpath)
+            if profile_dir.is_symlink():
+                realpath = profile_dir.resolve()
+                profile_dir.unlink()
+                shutil.rmtree(str(realpath))
             else:
-                shutil.rmtree(profile_dir)
+                shutil.rmtree(str(profile_dir))
         except FileNotFoundError:
             msg = "The removal of profile {} failed".format(name)
             log.error(msg, exc_info=True)
