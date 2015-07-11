2f091f812fcd4d9fc1615d38cf0274e5866412d8
Francesco Montesano <franz.bergesund@gmail.com>
2015-07-11 21:02:49 +0200

    test execution order of pytest

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -3,12 +3,26 @@
 Copyright (c) 2015 Francesco Montesano
 MIT Licence
 """
+import os
 
-import dodocs
+# import dodocs
 
 
 def test_list():
     "List profiles"
     command_line = 'profile list'
+    print(command_line)
+    print(os.path.expanduser('~'))
+    # dodocs.main(command_line.split())
 
-    dodocs.main(command_line.split())
+
+def test_tmp(tmp_homedir):
+    print(os.path.expanduser('~'))
+
+
+def test_create():
+    "List profiles"
+    command_line = 'profile create'
+    print(command_line)
+    print(os.path.expanduser('~'))
+    # dodocs.main(command_line.split())
