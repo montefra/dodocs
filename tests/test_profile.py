2a6bff10ab663160d18f8fb9a55ecaa5eaafa673
Francesco Montesano <franz.bergesund@gmail.com>
2015-07-12 12:32:45 +0200

    Now I think I understand (better) the fixtures

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -5,24 +5,25 @@ MIT Licence
 """
 import os
 
-# import dodocs
+import dodocs.utils as du
 
 
 def test_list():
     "List profiles"
     command_line = 'profile list'
     print(command_line)
-    print(os.path.expanduser('~'))
+    print(du.dodocs_directory())
     # dodocs.main(command_line.split())
 
 
 def test_tmp(tmp_homedir):
     print(os.path.expanduser('~'))
+    print(du.dodocs_directory())
 
 
 def test_create():
     "List profiles"
     command_line = 'profile create'
     print(command_line)
-    print(os.path.expanduser('~'))
+    print(du.dodocs_directory())
     # dodocs.main(command_line.split())
