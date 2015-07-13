152f5b304b30e75b9e83390e9f1799810ff79861
Francesco Montesano <franz.bergesund@gmail.com>
2015-07-13 19:18:15 +0200

    Run `profile list` and skip `profile create`

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -3,27 +3,19 @@
 Copyright (c) 2015 Francesco Montesano
 MIT Licence
 """
-import os
+import pytest
 
-import dodocs.utils as du
+import dodocs
 
 
 def test_list():
     "List profiles"
     command_line = 'profile list'
-    print(command_line)
-    print(du.dodocs_directory())
-    # dodocs.main(command_line.split())
+    dodocs.main(command_line.split())
 
 
-def test_tmp(tmp_homedir):
-    print(os.path.expanduser('~'))
-    print(du.dodocs_directory())
-
-
+@pytest.mark.skipif("True")
 def test_create():
     "List profiles"
     command_line = 'profile create'
-    print(command_line)
-    print(du.dodocs_directory())
     # dodocs.main(command_line.split())
