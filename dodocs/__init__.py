7717aad873f7cc68de26618c49d24cd5dc6202c5
Francesco Montesano <franz.bergesund@gmail.com>
2015-07-15 09:04:29 +0200

    Remove colorama and update the logger interface

--- dodocs/__init__.py
+++ dodocs/__init__.py
@@ -7,14 +7,10 @@ MIT Licence
 import os
 import sys
 
-import colorama
-
 from dodocs.cmdline import parse
-from dodocs.logger import setLogger
+import dodocs.logger as dlog
 
 __version__ = "0.0.1"
-
-colorama.init(autoreset=True)
 
 
 def main(argv=None):
@@ -28,20 +24,25 @@ def main(argv=None):
     """
     args = parse(argv=argv)
 
-    log = setLogger(args)
+    dlog.setLogger(args)
+    # make sure to reset the subcommand name
+    dlog.set_subcommand(args)
+    log = dlog.getLogger()
 
     if "func" in args:
         args.func(args)
         log.debug("Finished")
+        return 0
     else:
         # defaults profile to list
         if args.subparser_name == 'profile' and args.profile_cmd is None:
             main(sys.argv[1:] + ["list"])
         else:
             # in the other cases suggest to run -h
-            msg = (colorama.Fore.RED + "Please provide a valid command.\n"
+            msg = ("Please provide a valid command.\n"
                    "Type\n  " + os.path.split(sys.argv[0])[1])
             if args.subparser_name is not None:
                 msg += " " + args.subparser_name
             msg += ' -h'
             log.error(msg)
+            return 1
