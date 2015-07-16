e1d8a1aa6ec802b10e1568ee5f8eddebd10ac216
Francesco Montesano <franz.bergesund@gmail.com>
2015-07-16 21:18:46 +0200

    Profile listing uses new logging implementation

--- dodocs/profiles/plist.py
+++ dodocs/profiles/plist.py
@@ -6,11 +6,9 @@ MIT Licence
 
 import os
 
-import colorama
-
 import dodocs.config as dconf
 from dodocs import utils
-from dodocs.logger import getLogger
+import dodocs.logger as dlog
 
 
 def plist(args):
@@ -21,33 +19,33 @@ def plist(args):
     args : namespace
         parsed command line arguments
     """
-    log = getLogger()
+    log = dlog.getLogger()
 
     log.debug("Listing profiles")
     dodocs_dir = utils.dodocs_directory()
 
     if not dodocs_dir.exists():
-        log.error("No dodocs directory found. Create it first with the command"
-                  " 'dodoc profile create [profilename]'")
+        log.critical("No dodocs directory found. Create it first with the"
+                     " command 'dodoc profile create [profilename]'")
         return
 
     dirpath, dirnames = next(os.walk(str(dodocs_dir)))[:2]
     if dirnames:
-        msg = colorama.Fore.GREEN + "Available profiles:" + colorama.Fore.RESET
+        log.info("Available profiles:")
         for d in dirnames:
             profile_dir = dodocs_dir / d
-            msg += "\n  * {}".format(d)
+            msg = "  * {}".format(d)
             if profile_dir.is_symlink():
                 msg += " (-> {})".format(profile_dir.resolve())
+            log.info(msg)
             try:
                 for project in dconf.get_projects(d):
-                    msg += "\n    + {}".format(project)
-            except dconf.DodocConfigError as e:
-                msg += ("\n    + " + colorama.Fore.RED + "there is a problem"
-                        " with the configuration file. See the above error"
-                        " log." + colorama.Fore.RESET)
-                log.error(e)
+                    dlog.set_profile("")
+                    log.info("    + %s", project)
+            except dconf.DodocConfigError:
+                msg = ("    + there is a problem with the configuration"
+                       " file.")
+                log.error(msg, exc_info=True)
 
-        print(msg)
     else:
-        print(colorama.Fore.RED + "No profile found")
+        log.warning("No profile found")
