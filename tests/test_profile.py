a1460b3b9bcd6c90aa7f61d3a771a3ad0fea03e5
Francesco Montesano <franz.bergesund@gmail.com>
2015-08-02 22:58:05 +0200

    Test listing non exising directory

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -8,6 +8,23 @@ import pytest
 import dodocs
 
 
+# fixtures
+@pytest.fixture
+def profile_list():
+    """Execute the command profile list"""
+    command_line = 'profile list'
+    dodocs.main(command_line.split())
+
+
+# profile listing
+def test_list_no_exist(tmp_homedir, profile_list, caplog):
+    """Test that listing without a dodocs directory fails"""
+    for record in caplog.records():
+        assert record.levelname == "CRITICAL"
+    assert "No dodocs directory found" in caplog.text()
+
+
+@pytest.mark.skipif("True")
 def test_list():
     "List profiles"
     command_line = 'profile list'
