5493848ddca54a57a6dceb781bd417a9232cf1c4
Francesco Montesano <franz.bergesund@gmail.com>
2015-08-07 08:44:48 +0200

    Add remove and edit on non existing dodocs directories

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -16,12 +16,44 @@ def profile_list():
     dodocs.main(command_line.split())
 
 
+@pytest.fixture
+def profile_rm():
+    """Execute the command profile remove"""
+    command_line = 'profile remove dodocs_pytest'
+    dodocs.main(command_line.split())
+
+
+@pytest.fixture
+def profile_edit():
+    """Execute the command profile edit"""
+    command_line = 'profile edit dodocs_pytest'
+    dodocs.main(command_line.split())
+
+
 # profile listing
 def test_list_no_exist(tmp_homedir, profile_list, caplog):
     """Test that listing without a dodocs directory fails"""
-    for record in caplog.records():
-        assert record.levelname == "CRITICAL"
-    assert "No dodocs directory found" in caplog.text()
+    record = caplog.records()[0]
+    assert record.levelname == "CRITICAL"
+    assert "No dodocs directory found" in record.message
+
+
+# profile removal
+def test_rm_no_exists(tmp_homedir, profile_rm, caplog):
+    """Test that removing without a dodocs directory print a warning as first
+    message"""
+    record = caplog.records()[0]
+    assert record.levelname == "WARNING"
+    assert "Profile does not exist" in record.message
+
+
+# profile editing
+def test_edit_no_exists(tmp_homedir, profile_edit, caplog):
+    """Test that removing without a dodocs directory print a warning as first
+    message"""
+    record = caplog.records()[0]
+    assert record.levelname == "CRITICAL"
+    assert "No dodocs directory found" in record.message
 
 
 @pytest.mark.skipif("True")
